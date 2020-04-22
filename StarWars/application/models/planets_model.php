a70f7f8856f192c290c67002237ad76bf7d17f3b
TGab99 <55789282+TGab99@users.noreply.github.com>
2020-04-22 19:41:25 +0200

    Planets model létrehozva

---
+++ StarWars/application/models/planets_model.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of planets_model
+ *
+ * @author Acer
+ */
+class planets_model extends CI_Model{
+    public function __construct() {
+        parent::__construct();
+        
+        $this->load->database();
+    }
+    
+    public function get_list(){
+        $this->db->select("*");
+        $this->db->from('planets');
+        
+        $this->db->order_by('id','ASC');
+        
+        $query = $this->db->get();
+        $result = $query->result();
+        
+        return $result;
+    }
+}
