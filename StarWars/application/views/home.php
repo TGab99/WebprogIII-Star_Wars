a51c523c30b1033a3066548e389429652de52a69
TGab99 <55789282+TGab99@users.noreply.github.com>
2020-05-24 19:19:02 +0200

    Átnevezve

---
+++ StarWars/application/views/home.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('style/home.css');?>">
+</head>
+<body>
+    <?php $this->load->view('header'); ?>
+    <h3>Welcome to my website!</h3>
+    <?php $this->load->view('footer'); ?>
+</body>
+</html>
