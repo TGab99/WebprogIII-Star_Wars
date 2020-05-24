f67f339eba11dde78b28536ebc456a49571e0f5f
TGab99 <55789282+TGab99@users.noreply.github.com>
2020-05-24 19:20:26 +0200

    Header view php fájl létrehozva

---
+++ StarWars/application/views/header.php
@@ -0,0 +1,6 @@
+<link rel="stylesheet" type="text/css" href="<?php echo base_url('style/header');?>">
+<header>
+    <h1>Star Wars</h1>
+    <h3>A long time ago in a galaxy far far away ...</h3>
+    <h4 id="logout"><a href="index">Logout</a></h3>
+</header>
