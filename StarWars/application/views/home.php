533a3002fd3aa8439951b416a436cc125772f916
TGab99 <55789282+TGab99@users.noreply.github.com>
2020-05-24 22:14:01 +0200

    Módosítva

--- StarWars/application/views/home.php
+++ StarWars/application/views/home.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <title>Star Wars</title>
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('style/home.css');?>">
 </head>
 <body>
@@ -24,6 +26,18 @@
     </div>
     
     <h3>Welcome to my website!</h3>
+    
+    <div class="quotes">
+    <h3>Quotes</h3>
+    
+    <blockquote cite="https://www.starwars.com/news/40-memorable-star-wars-quotes">
+        <p><q>Do. Or do not. There is no try.</q> - Yoda</p>
+        <p><q>No. I am your father.</q> - Dart Vader</p>
+        <p><q>I’ll never turn to the dark side. You’ve failed, your highness. I am a Jedi, like my father before me.</q> - Luke Skywalker</p>
+        <p><q>Fear is the path to the dark side. Fear leads to anger; anger leads to hate; hate leads to suffering. I sense much fear in you.</q> - Yoda</p>
+        <p><q>Chewie, we’re home.</q> - Han Solo</p>
+    </blockquote>
+    </div>
     
     <?php $this->load->view('footer'); ?>
 </body>
