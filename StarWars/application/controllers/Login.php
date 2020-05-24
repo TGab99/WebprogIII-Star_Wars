174dbd41274c3a946e917858cbd7dbad575a9fa2
TGab99 <55789282+TGab99@users.noreply.github.com>
2020-05-24 15:30:50 +0200

    Login controller létrehozva

---
+++ StarWars/application/controllers/Login.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Login
+ *
+ * @author Acer
+ */
+class Login extends CI_Controller{
+    public function __construct() { 
+         parent::__construct(); 
+         
+         $this->load->model('login_model');
+         
+         $this->load->helper(array('form', 'url')); 
+      } 
+	
+      public function index() {
+         $this->load->library('form_validation');
+	
+	 $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
+	 $this->form_validation->set_rules('password', 'Password', 'required');	 
+			
+         if ($this->form_validation->run() == FALSE) { 
+            $this->load->view('login_form'); 
+         } 
+         else { 
+			$result = $this->login_model->login();
+			if ($result > 0)
+			redirect('dashboard');
+			else 
+			  { 
+				$msg = "The email or password you entered is incorrect.";
+				redirect(base_url('index'));
+			  } 
+		}
+	} 
+		
+	public function dashboard()
+	  {
+	    $this->load->view('dashboard');	
+	  }
+}
